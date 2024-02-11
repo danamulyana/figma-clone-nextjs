2d2811fdb20a155391021f1224661377f9fbfbd8
Dana Mulyana <danabontot@gmail.com>
2024-02-11 20:58:24 +0700

    added realtime shape

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,23 +1,77 @@
-import { useMyPresence, useOthers } from "@/liveblocks.config";
+import { useBroadcastEvent, useEventListener, useMyPresence, useOthers } from "@/liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
 import { useCallback, useEffect, useState } from "react";
 import CursorChat from "./cursor/CursorChat";
-import { CursorMode, CursorState } from "@/types/type";
+import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
+import ReactionSelector from './reaction/ReactionButton'
+import FlyingReaction from "./reaction/FlyingReaction";
+import useInterval from "@/hooks/useInterval";
 
-const Live = () => {
+type Props = {
+    canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+}
+
+const Live = ({ canvasRef } : Props) => {
     const others = useOthers();
     const [{ cursor }, updateMyPresence] = useMyPresence() as any;
     const [cursorState, setCursorState] = useState<CursorState>({
         mode: CursorMode.Hidden,
     })
+    const [reaction, setReaction] = useState<Reaction[]>([]);
+
+    const broadcast = useBroadcastEvent();
+
+    useInterval(() => {
+        setReaction((reaction) => reaction.filter((r) => r.timestamp > Date.now() - 4000))
+    },1000)
+
+    useInterval(() => {
+        if(cursorState.mode === CursorMode.Reaction &&
+            cursorState.isPressed && cursor){
+                setReaction((reaction) => reaction.concat([
+                    {
+                        point: {
+                            x: cursor.x,
+                            y: cursor.y,
+                        },
+                        value: cursorState.reaction,
+                        timestamp: Date.now(),
+                    }
+                ]))
+
+                broadcast({
+                    x: cursor.x,
+                    y: cursor.y,
+                    value: cursorState.reaction,
+                })
+            }
+    },100);
+
+    useEventListener((eventData) => {
+        const event = eventData.event as ReactionEvent;
+
+        setReaction((reaction) => reaction.concat([
+            {
+                point: {
+                    x: event.x,
+                    y: event.y,
+                },
+                value: event.value,
+                timestamp: Date.now(),
+            }
+        ]))
+    })
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
 
-        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+        if(cursor == null || cursorState.mode !== CursorMode.ReactionSelector) {
+            const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+            const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+            
+            updateMyPresence({ cursor: {x,y} });
+        }
 
-        updateMyPresence({ cursor: {x,y} });
     },[])
 
     const handlePointerLeave = useCallback((event: React.PointerEvent) => {
@@ -30,7 +84,20 @@ const Live = () => {
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
         updateMyPresence({ cursor: {x,y} });
-    },[])
+
+        setCursorState((state : CursorState) => 
+            cursorState.mode === CursorMode.Reaction ? 
+            {...state, isPressed: true} : state
+        )
+    },[cursorState.mode, setCursorState])
+
+    const handlePointerUp = useCallback((event: React.PointerEvent) => {
+        setCursorState((state : CursorState) => 
+            cursorState.mode === CursorMode.Reaction ? 
+            {...state, isPressed: true} : state
+        )
+    },[cursorState.mode, setCursorState])
+
 
     useEffect(() => {
         const onKeyUp = (e: KeyboardEvent) => {
@@ -43,6 +110,11 @@ const Live = () => {
             } else if(e.key === 'Escape'){
                 updateMyPresence({ message: '' })
                 setCursorState({ mode: CursorMode.Hidden});
+            } else if(e.key === 'e'){
+                updateMyPresence({ message: '' })
+                setCursorState({ 
+                    mode: CursorMode.ReactionSelector
+                });
             }
         }
 
@@ -61,21 +133,48 @@ const Live = () => {
         };
     }, [updateMyPresence]);
 
+    const setReactions = useCallback((reaction: string) => {
+        setCursorState({ 
+            mode: CursorMode.Reaction,
+            reaction, 
+            isPressed: false
+        })
+    },[])
+
     return(
         <div
+            id="canvas"
             onPointerMove={handlePointerMove}
             onPointerLeave={handlePointerLeave}
             onPointerDown={handlePointerDown}
-            className="h-[100vh] w-full flex justify-center items-center text-center"
+            onPointerUp={handlePointerUp}
+            className="h-full w-full flex flex-1 justify-center items-center"
         >
-            <h1 className="text-2xl text-white">Liveblocks Figma clone</h1>
+            
+            <canvas ref={canvasRef} />
 
+            {reaction.map((r) => (
+                <FlyingReaction 
+                    key={r.timestamp.toString()}
+                    x={r.point.x}
+                    y={r.point.y}
+                    timestamp={r.timestamp}
+                    value={r.value}
+                />
+            ))}
+            
             {cursor && (
                 <CursorChat 
                     cursor={cursor}
                     cursorState={cursorState}
                     setCursorState={setCursorState}
                     updateMyPresence={updateMyPresence}
+                />
+            )}
+
+            {cursorState.mode === CursorMode.ReactionSelector && (
+                <ReactionSelector 
+                    setReaction={setReactions}
                 />
             )}
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+            canvas: "commonjs canvas",
+        });
+
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'liveblocks.io',
+                port: ''
+            }
+        ]
+    }
+    
+};
 
 export default nextConfig;
